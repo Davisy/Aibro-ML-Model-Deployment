09e56416b3a37166e242b1f38166331ec6f523a6
Davis David <davisdavid179@gmail.com>
2022-01-27 10:33:54 +0300

    Update predict.py

--- sentiment_model_repo/predict.py
+++ sentiment_model_repo/predict.py
@@ -1,15 +1,16 @@
 # import important modules
 
-import json # load data
-import joblib # load model 
-from clean import text_cleaning # function to clean the text 
-  
-    
+import json  # load data
+import joblib  # load model
+from clean import text_cleaning  # function to clean the text
+
+
 def load_model():
-    #load model 
+    # load model
     model = joblib.load("model/sentiment_model_pipeline.pkl")
-    
+
     return model
+
 
 def run(model):
     fp = open("data/data.json", "r")
@@ -18,6 +19,7 @@ def run(model):
 
     result = {"data": model.predict([review])}
     return result
+
 
 if __name__ == "__main__":
     run(load_model())
